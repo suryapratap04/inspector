4b1079e69c46f22cc83d2e70bb3db9514c20c2b5
chelojimenez <marcelojimenezrocabado@gmail.com>
2025-07-07 22:38:08 -0700

    created the base infrastructure for the MCPProxyService extraction

---
+++ server/src/shared/MCPProxyService.ts
@@ -0,0 +1,4 @@
+// MCPProxyService implementation - to be completed in Step 3
+export class MCPProxyService {
+  // Implementation will be added in Step 3
+}
---
+++ server/src/shared/TransportFactory.ts
@@ -0,0 +1,4 @@
+// TransportFactory implementation - to be completed in Step 2
+export class TransportFactory {
+  // Implementation will be added in Step 2
+}
