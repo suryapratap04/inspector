b1d7bb901112c542c439acfb0ce9283183e81706
Surya Pratap Singh <suryapratapsingh.cse26@jecrc.ac.in>
2025-07-28 15:28:05 +0530

    Add Interface oauthTokens

--- src/hooks/use-app-state.ts
+++ src/hooks/use-app-state.ts
@@ -13,10 +13,19 @@ import {
 } from "@/lib/types";
 import { useLogger } from "./use-logger";
 
+export interface oauthTokens {
+  client_id: string;
+  client_secret: string;
+  access_token: string;
+  refresh_token: string;
+  expires_in: number;
+  scope: string;
+}
+
 export interface ServerWithName {
   name: string;
   config: MastraMCPServerDefinition;
-  oauthTokens?: any;
+  oauthTokens?: oauthTokens;
   lastConnectionTime: Date;
   connectionStatus:
     | "connected"
